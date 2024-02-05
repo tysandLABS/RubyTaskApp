e8262ae3f7a72fa9ca6a6b1de40bd445b880f4d1
Tyrone Sanderson <134633090+tysandLABS@users.noreply.github.com>
2024-02-05 13:44:01 -0500

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,11 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
+  config.hosts.clear 
+
+  config.action_mailer.default_url_options = { host: '0.0.0.0', protocol: 'https' }
+
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
